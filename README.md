9a86b6286d4f7dec139f09c8e4beba781c8630db
Kate Birmingham <kabi6686@colorado.edu>
2018-03-11 19:52:45 -0600

    added comments to JS and text to README

--- README.md
+++ README.md
@@ -6,7 +6,11 @@ A node application for purchasing items in an Amazon-type atmosphere.
 
 ## How It Works
 
-Upon running the associated file in Node (bamazonCustomer), the user will be prompted with a series of prompts from inquirer. Initially the user will be asked if they would like to purchase an item, and if they respond with 'Yes', a list of items along with their price and stock availability will be displayed. Another prompt will ask the user which item they would like to buy by name, followed by another prompt regarding how many units. If their unit request does not exceed the stock quantity for that product, their order will be processed and the inventory database in MySQL will be updated, and the total price will be displayed to the user. After purchasing an item, the user will be rerouted back to the initial prompt if they would like to purchase an item. 
+Upon running the associated file in Node (bamazonCustomer), the user will be prompted with a series of prompts from inquirer. 
+
+Initially the user will be asked if they would like to purchase an item, and if they respond with 'Yes', a list of items along with their price and stock availability will be displayed. Another prompt will ask the user which item they would like to buy by name, followed by another prompt regarding how many units. If their unit request does not exceed the stock quantity for that product, their order will be processed and the inventory database in MySQL will be updated. 
+
+The total price will then be displayed to the user. After purchasing an item, the user will be rerouted back to the initial prompt if they would like to purchase another item. 
 
 ## Technologies Utilized
 	- NodeJS
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,13 +1,16 @@
+// requiring npm packages
 var inquirer = require("inquirer");
 var mysql = require("mysql");
 var Table = require("cli-table");
 
+
+// connection to the database
 var connection = mysql.createConnection({
 	host: "localhost",
 	port: 8889,
 
 	//username
-	user: "root",
+	user: "root", 
 
 	//password
 	password: "root",
@@ -15,12 +18,15 @@ var connection = mysql.createConnection({
 
 });
 
+
+// after connecting to the DB, do this function
 connection.connect(function(err){
 	if (err) throw err;
 	start();
 
 });
 
+// start function asks user if they want buy an item
 function start() {
 	inquirer.prompt ({
 		name: "start",
@@ -28,10 +34,14 @@ function start() {
 		message: "Would you like to purchase an item?"
 	})
 	.then(function(answer){
+		// if user responds with yes
 		if (answer.start === true) {
 			console.log("\nStart Shopping Now!");
+			// displays table of items for sale
 			showItems();
+		// if user responds with no
 		} else {
+			// reroutes to intial start prompt if user declines
 			console.log("\nCome Back Soon!\n")
 			start();
 		}
@@ -41,17 +51,27 @@ function start() {
 
 } // END START FUNCTION
 
+// showItems function displays items in database as a table
 function showItems () {
+	// creates table constructor
 	var table = new Table ({
+		//columns to be displayed in table
 		head: ['Item id', 'Product Name', 'Price', "Stock Availability"]
+		// column widths
 		, colWidths: [12, 30, 30, 40]
 	});
+	// connect to database, select everything from products table
 	connection.query("SELECT * FROM products", function(err, res) {
+	// loop through every item in products table
 	for (var i = 0 ; i < res.length; i++ ) {
+		// push each product to table constructor 
 		table.push([res[i].id, res[i].product_name, "$ " + res[i].price, res[i].stock_quantity])
 	}
+
+	// logs table to console in string format
 	console.log(table.toString());
 
+	//run buyItem function
 	buyItem();
   }); 
 
@@ -59,6 +79,8 @@ function showItems () {
 
 } // END buyItem FUNCTION
 
+
+// buyItem function allows user to buy item from table
 function buyItem() {
 	inquirer.prompt([
 	{
@@ -73,20 +95,24 @@ function buyItem() {
 	}
 	]).then(function(answers){
 		console.log("\nYour purchase is being processed...\n");
-
+		// select everything from products table where product_name = user response
 		connection.query("SELECT * FROM products WHERE ?", 
 			{
 				product_name: answers.choose_item.toLowerCase().trim()
 			}, 
 			function(err, res) {
+			// if the quantity the user wants is less or equal to stock in database
 			if (answers.itemQuantity <= res[0].stock_quantity) {
 				console.log("--------------------------");
 				console.log("Thank you for your order");
 				var quantity = answers.itemQuantity;
 				var response = answers.choose_item;
+				// run updateInventory function with quantity, response arguments
 				updateInventory(quantity, response);
 			} else {
+				// user specified quantity above stock quantity
 				console.log("This order cannot be processed as your desired quantity exceeds our current stock.")
+				// reroutes to buyItem function
 				buyItem();
 			}
 		}
@@ -94,17 +120,21 @@ function buyItem() {
 	})
 } // END BUY ITEM FUNCTION
 
-function updateInventory (quantity, response) {
 
+// updateInventory function updates inventory in database after user purchases item
+function updateInventory (quantity, response) {
+	// selects everything from products table where product_name = user response 
 	connection.query("SELECT * FROM products WHERE ?", 
 		{
 			product_name: response
 		},
 		function(err, res) {
+			// 
 			var stock = res[0].stock_quantity;
 			var price = res[0].price;
 			var total = price * quantity;
 
+			// tells user total cost of purchase
 			console.log("\nYour total is: " + total);
 			console.log("--------------------------");
 
@@ -117,13 +147,15 @@ function updateInventory (quantity, response) {
 						product_name: response
 					}
 				],
+				// if inventory successfully updated, run this function
 				function(err, res) {
 					if (err) throw err;
 					console.log("\nInventory Updated!");
+					// after purchasing item, back to start
 					start();
 				}
-			   )
+			)
 	 })
 
-} // END CHECK QUANTITY FUNCTION
+} // END updateInventory FUNCTION
 
